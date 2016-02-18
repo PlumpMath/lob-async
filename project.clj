46dce70bbb52948176ee6a9598741bfb14926869
Neel Upadhyaya <neel.upadhyaya@eckoh.com>
2016-02-18 10:28:10 +0000

    Added lob dep

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.async "0.2.374"]
-                 [com.stuartsierra/component "0.3.1"]])
+                 [com.stuartsierra/component "0.3.1"]
+                 [totalperspective/lob "0.1.2"]])
